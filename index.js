b30cbbb90676ccf5425ce5bf3a1ee5a184b9db01
Cindy Qu <cindyqu42@gmail.com>
2022-08-29 15:12:50 -0700

    commit tree 47e8632ef373aceae7bd6687a80c6c88b61ca6bc

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber) {
   // Write your code here!
+  if (someNumber <= 400) {
+    return `This one is on me!`;
+  }
+  else if (someNumber > 400 && someNumber < 2000) {
+    return `That will be twenty bucks.`;
+  }
+  else if (someNumber > 2000 && someNumber < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (someNumber > 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(someCity) {
   // Write your code here!
+  return (someCity === 'NYC') ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(someAmount) {
   // Write your code here!
+  switch (someAmount) {
+    case `generous`: return "Thank you so much.";
+
+    case `not as generous`: return "Thank you.";
+
+    default: return 'Bye.';
+  }
 }
